39c04de2c7238ec6e956016ba9f767be932a7242
Carlos1515Santana <36937459+Carlos1515Santana@users.noreply.github.com>
2019-06-14 16:44:39 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,13 +14,12 @@ import { SalaService } from './sala.service';
 export const firebaseConfig = {
   apiKey: "AIzaSyDJee8qliYkcYVMIG57-w8mHHO7Vv1fvUM",
   authDomain: "chat-16432.firebaseapp.com",
-  databaseURL: "https://chat-16432.firebaseio.com",
+  databaseURL: "https://ccp3anmca-deswebmob-chat.firebaseio.com",
   projectId: "chat-16432",
   storageBucket: "chat-16432.appspot.com",
   messagingSenderId: "104458173944",
   appId: "1:104458173944:web:35edf63bbe9d2af7"
 };
-
 @NgModule({
   declarations: [
     MyApp,
